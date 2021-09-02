aadd6901933f28b91ca253644620ed7ddd94e81b
priyam314 <garg.priyam2015@gmail.com>
2021-09-02 13:16:16 +0530

    used sessions

--- learnTemplates/pkg/handler/handlers.go
+++ learnTemplates/pkg/handler/handlers.go
@@ -23,12 +23,18 @@ func NewHandlers(r *Repository){
 	Repo = r
 }
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request){
+	// gives ip address
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	render.RenderTemplate(w,"home.page.tmpl", &models.TemplateData{})
 }
 func (m *Repository) About(w http.ResponseWriter, r *http.Request){
 	// business logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 	render.RenderTemplate(w,"about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
